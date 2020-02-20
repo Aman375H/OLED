3a309bb78ebd9ccc5f415955476424a38aee6d8b
Kaleb <kaleb.zeray@gmail.com>
2020-02-20 15:44:55 -0500

    Configured workspace for VS_Code

--- Scrolling_Text.ino
+++ Scrolling_Text.ino
@@ -10,23 +10,26 @@
 
 CRGB leds[NUM_LEDS];
 
-
-uint16_t XY( uint8_t x, uint8_t y)
+uint16_t XY(uint8_t x, uint8_t y)
 {
   uint16_t i;
-  if( x & 0x01) {
+  if (x & 0x01)
+  {
     // Odd rows run backwards
     uint8_t reverseY = (STRIP_Y - 1) - y;
     i = (x * STRIP_Y) + reverseY;
-  } else {
+  }
+  else
+  {
     // Even rows run forwards
     i = (x * STRIP_Y) + y;
   }
-  
+
   return i;
 }
 
-void setup() {
+void setup()
+{
   FastLED.addLeds<STRIP_TYPE, LED_PIN, GRB>(leds, NUM_LEDS);
 
   // initialize the serial communication:
@@ -35,30 +38,37 @@ void setup() {
   pinMode(11, INPUT); // Setup for leads off detection LO -
 }
 
-void loop() { 
+void loop()
+{
   // code to get text from translate API
   char *text = "A";
   printString(text);
 }
 
-void printChar(char c) {
+void printChar(char c)
+{
   char *character_data = character_array[c];
   int row_count = 1;
-  
-  while(row_count-1 != character_data[0]) {
-    for (int j = 0; j < 8; j++) {
-      if(character_data[j*row_count+1+8*(row_count-1)] == 1)
-        leds[j] = CRGB(255*BRIGHTNESS, 0, 0);
+
+  while (row_count - 1 != character_data[0])
+  {
+    for (int j = 0; j < 8; j++)
+    {
+      if (character_data[j * row_count + 1 + 8 * (row_count - 1)] == 1)
+        leds[j] = CRGB(255 * BRIGHTNESS, 0, 0);
     }
-    row_count+=1;
-    
+    row_count += 1;
+
     CRGB ledsCopy[NUM_LEDS];
-    memcpy(ledsCopy, leds, sizeof(leds[0])*NUM_LEDS);
-  
-    for (int x = 0; x < STRIP_X-1; x++) {
-      for (int y = 0; y < STRIP_Y; y++) {
-        if (ledsCopy[XY (x,y)] == CRGB(255*BRIGHTNESS,0,0)) {
-          leds[XY (x+1, y)] = ledsCopy[XY (x, y)];
+    memcpy(ledsCopy, leds, sizeof(leds[0]) * NUM_LEDS);
+
+    for (int x = 0; x < STRIP_X - 1; x++)
+    {
+      for (int y = 0; y < STRIP_Y; y++)
+      {
+        if (ledsCopy[XY(x, y)] == CRGB(255 * BRIGHTNESS, 0, 0))
+        {
+          leds[XY(x + 1, y)] = ledsCopy[XY(x, y)];
         }
       }
     }
@@ -67,8 +77,10 @@ void printChar(char c) {
   }
 }
 
-void printString(char* s) {
-  while (*s != 0) {
+void printString(char *s)
+{
+  while (*s != 0)
+  {
     printChar(*s);
     s++;
   }
--- heartbeat_display.ino
+++ heartbeat_display.ino
@@ -16,90 +16,103 @@ int pulseVals[STRIP_X];
 
 CRGB leds[NUM_LEDS];
 
-
-uint16_t XY( uint8_t x, uint8_t y)
+uint16_t XY(uint8_t x, uint8_t y)
 {
   uint16_t i;
-  if( x & 0x01) {
+  if (x & 0x01)
+  {
     // Odd rows run backwards
     uint8_t reverseY = (STRIP_Y - 1) - y;
     i = (x * STRIP_Y) + reverseY;
-  } else {
+  }
+  else
+  {
     // Even rows run forwards
     i = (x * STRIP_Y) + y;
   }
-  
+
   return i;
 }
 
-void setup() {
+void setup()
+{
   FastLED.addLeds<STRIP_TYPE, LED_PIN, GRB>(leds, NUM_LEDS);
 
   // initialize the serial communication:
   Serial.begin(9600);
   pinMode(10, INPUT); // Setup for leads off detection LO +
   pinMode(11, INPUT); // Setup for leads off detection LO -
-  
-//  for (int i = 0; i < STRIP_X-1; i++) {
-//    pulseVals[i] = floor(STRIP_X/2);
-//  }
+
+  //  for (int i = 0; i < STRIP_X-1; i++) {
+  //    pulseVals[i] = floor(STRIP_X/2);
+  //  }
 }
 
-void loop() { 
+void loop()
+{
   Signal = analogRead(pulsePin);
   Serial.println(Signal);
   //int beat = round(abs((analogRead(A0) - 200)*0.0114));  // assuming from 200 to 900
   int raw_beat = Signal;
   int beat = round(abs((raw_beat)*0.0114));
-  
-  leds[beat] = CRGB(255*BRIGHTNESS, 0, 0);
 
-  CRGB ledsCopy[NUM_LEDS]; 
-  memcpy(ledsCopy, leds, sizeof(leds[0])*NUM_LEDS);
+  leds[beat] = CRGB(255 * BRIGHTNESS, 0, 0);
+
+  CRGB ledsCopy[NUM_LEDS];
+  memcpy(ledsCopy, leds, sizeof(leds[0]) * NUM_LEDS);
 
   //pulseVals[0] = beat;
-  
-  int bkg_beat = floor(raw_beat*0.2);
-  for (int x = 0; x < STRIP_X; x++) {
-    for (int y = 0; y < STRIP_Y; y++) {
-      leds[XY (x, y)] = CRGB(bkg_beat*BRIGHTNESS,0,(255-bkg_beat)*BRIGHTNESS);
+
+  int bkg_beat = floor(raw_beat * 0.2);
+  for (int x = 0; x < STRIP_X; x++)
+  {
+    for (int y = 0; y < STRIP_Y; y++)
+    {
+      leds[XY(x, y)] = CRGB(bkg_beat * BRIGHTNESS, 0, (255 - bkg_beat) * BRIGHTNESS);
     }
   }
 
-  for (int x = 0; x < STRIP_X-1; x++) {
-    for (int y = 0; y < STRIP_Y; y++) {
-//    if (pulseVals[x] > 3) {
-//        for (int i = pulseVals[x]-1; i > 3; i--) {
-//          leds[XY (x, pulseVals[x]+i)] = CRGB((255-bkg_beat)*BRIGHTNESS,0,bkg_beat*BRIGHTNESS);
-//        }
-//      }
-//      else if (pulseVals[x] <= 3) {
-//        for (int j = pulseVals[x]; j < 4; j++) {
-//          leds[XY (x, pulseVals[x]-j)] = CRGB((255-bkg_beat)*BRIGHTNESS,0,bkg_beat*BRIGHTNESS);
-//        }
-//      }
- //   leds[XY (x, pulseVals[x])] = ;
-    
-      if (ledsCopy[XY (x,y)] == CRGB(255*BRIGHTNESS,0,0)) {
-        leds[XY (x+1, y)] = ledsCopy[XY (x, y)];   // test this without making copy
+  for (int x = 0; x < STRIP_X - 1; x++)
+  {
+    for (int y = 0; y < STRIP_Y; y++)
+    {
+      //    if (pulseVals[x] > 3) {
+      //        for (int i = pulseVals[x]-1; i > 3; i--) {
+      //          leds[XY (x, pulseVals[x]+i)] = CRGB((255-bkg_beat)*BRIGHTNESS,0,bkg_beat*BRIGHTNESS);
+      //        }
+      //      }
+      //      else if (pulseVals[x] <= 3) {
+      //        for (int j = pulseVals[x]; j < 4; j++) {
+      //          leds[XY (x, pulseVals[x]-j)] = CRGB((255-bkg_beat)*BRIGHTNESS,0,bkg_beat*BRIGHTNESS);
+      //        }
+      //      }
+      //   leds[XY (x, pulseVals[x])] = ;
+
+      if (ledsCopy[XY(x, y)] == CRGB(255 * BRIGHTNESS, 0, 0))
+      {
+        leds[XY(x + 1, y)] = ledsCopy[XY(x, y)]; // test this without making copy
       }
 
-      if (beat > 3) {
-        for (int i = beat-1; i > 3; i--) {
-          leds[XY (0, i)] = CRGB(255*BRIGHTNESS,0,0);
+      if (beat > 3)
+      {
+        for (int i = beat - 1; i > 3; i--)
+        {
+          leds[XY(0, i)] = CRGB(255 * BRIGHTNESS, 0, 0);
         }
       }
-      else if (beat <= 3) {
-        for (int j = beat; j < 4; j++) {
-          leds[XY (0, j)] = CRGB(255*BRIGHTNESS,0,0);
+      else if (beat <= 3)
+      {
+        for (int j = beat; j < 4; j++)
+        {
+          leds[XY(0, j)] = CRGB(255 * BRIGHTNESS, 0, 0);
         }
       }
     }
   }
 
-    FastLED.show();
-    delay(100);
+  FastLED.show();
+  delay(100);
 
-//    for (int i = STRIP_X-2; i > 0; i--)
-//      pulseVals[i+1] = pulseVals[i];
+  //    for (int i = STRIP_X-2; i > 0; i--)
+  //      pulseVals[i+1] = pulseVals[i];
 }
